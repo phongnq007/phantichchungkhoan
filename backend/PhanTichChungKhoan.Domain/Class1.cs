42aecf0505751e42c48a4463668d15b150ffed38
phongnq007 <khongbietdau987@outlook.com>
2021-11-10 10:19:48 +0700

    Revert "init source code"
    This reverts commit 181586525a17d153fbc48ef4ba8d5c1cbab5b455.

--- backend/PhanTichChungKhoan.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PhanTichChungKhoan.Domain
-{
-    public class Class1
-    {
-    }
-}
--- frontend/src/components/NotFound/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFoundPage = () => {
-return (
-    <h2>This is NotFound page</h2>
-);
-};
-
-export default NotFoundPage;
